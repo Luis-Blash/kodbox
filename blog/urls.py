8b6ee2cce6a82f8b2a3535889f8de44cfd29197b
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2020-09-04 19:38:51 -0500

    Se modifico las istas de noticas y noticias individuales, Se hizo la paginacion y se hizo su html

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("noticias/", views.noticias, name="noticias"),
-    path("noticia/<int:noticia_id>", views.noticia, name="noticia")
+    path("noticia/<int:noticia_id>", views.noticia, name="noticia"),
+    path("categorias/<int:categoria_id>", views.categorias, name="categoria")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Noticias
-
+from django.core.paginator import Paginator
+from blog.models import Noticias, Categoria
 # Create your views here.
 def noticias(request):
 
     noticias_secundarias =  Noticias.objects.filter(publico=True).order_by("-creado_el")[1:]
     noticia_principal = Noticias.objects.filter(publico=True).order_by("-creado_el").first()
+    # paginar noticias, que es cuantas mostrar
+    paginacion = Paginator(noticias_secundarias,2)
+    # recoger numero de la pagina
+    pagina = request.GET.get('page')
+    pagina_noticia = paginacion.get_page(pagina)
 
     return render(request,"noticias/lista_noticias.html",{
         "title":"Noticias",
         "noticia_principal":noticia_principal,
-        "noticias_secundarias":noticias_secundarias
+        "noticias_secundarias":pagina_noticia
     })
 
 # noticias separadas
@@ -20,4 +25,22 @@ def noticia(request, noticia_id):
 
     return render(request, 'noticias/noticia-individual.html',{
         "noticia":noticia
+    })
+
+def categorias(request, categoria_id):
+
+    categorias_individual = get_object_or_404(Categoria, id = categoria_id )
+    
+    noticia_principal = Noticias.objects.filter(publico=True,categorias=categoria_id).order_by("-creado_el").first()
+    noticias_secundarias =  Noticias.objects.filter(publico=True,categorias=categoria_id).order_by("-creado_el")[1:]
+
+    # Paginacion
+    paginacion = Paginator(noticias_secundarias,2)
+    pagina = request.GET.get('page')
+    pagina_noticia = paginacion.get_page(pagina)
+
+    return render(request, "categorias/categorias-indivudual.html",{
+        "categoria": categorias_individual,
+        "noticia_principal":noticia_principal,
+        "noticias_secundarias":pagina_noticia
     })
